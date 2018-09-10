4fe268fe14264b7d4fed21e14a202c4a80db03e3
Saib Ahmed <saibahmed1512@gmail.com>
2018-09-10 23:00:08 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter , Route , Switch} from 'react-router-dom';
+import {BrowserRouter , Route , Switch , Link} from 'react-router-dom';
 import Home from "./components/Home";
+import One from "./components/One";
 import Contact from "./components/Contact";
+
 class App extends Component {
   render() {
     return (
@@ -11,13 +13,20 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React Router Class</h1>
+
         </header>
+
         <BrowserRouter>
+        <div>
           <Switch>
           <Route path="/home" component={Home}/>
           <Route path="/contact" component={Contact}/>
-
+          <Route path="/one" component={One}/>
+            <ul>
+            <li><Link to="/One">One</Link></li>
+            </ul>
           </Switch>
+        </div>
         </BrowserRouter>
 
       </div>
---
+++ src/components/One.js
@@ -0,0 +1,11 @@
+import React , {Component} from "react";
+class One extends Component {
+  render(){
+    return(
+      <div>
+        <h1>I am One Component</h1>
+      </div>
+    );
+  }
+}
+export default One;
